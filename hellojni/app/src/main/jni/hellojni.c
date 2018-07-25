0c2ff2f6ac6e62b8b5172c7a5b06701299c19e7b
dexterleslie@gmail.com <dexterleslie@gmail.com>
2018-07-25 15:38:35 +0800

    hellojni项目测试jni打印调试日志信息

--- hellojni/app/src/main/jni/hellojni.c
+++ hellojni/app/src/main/jni/hellojni.c
@@ -1,6 +1,10 @@
 #include <jni.h>
+#include <android/log.h>
+
+#define TAG "hellojni"
 
 JNIEXPORT jstring JNICALL Java_com_xy_android_hellojni_HelloJni_stringFromJNI
                     (JNIEnv *env){
+    __android_log_print(ANDROID_LOG_DEBUG,TAG ,"调试打印信息");
     return (*env)->NewStringUTF(env, "Hello World! I am Native interface");
 }
