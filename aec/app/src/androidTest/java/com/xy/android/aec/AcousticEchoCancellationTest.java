bb97f86524b7a0b1268adecc870f134b219a7b1c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2018-07-26 12:43:16 +0800

    新增aec项目，研究怎么实现android平台aec功能

---
+++ aec/app/src/androidTest/java/com/xy/android/aec/AcousticEchoCancellationTest.java
@@ -0,0 +1,89 @@
+package com.xy.android.aec;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.media.audiofx.AcousticEchoCanceler;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.LinkedList;
+
+@RunWith(AndroidJUnit4.class)
+public class AcousticEchoCancellationTest {
+    @Rule
+    public final PermissionsRule permissionsRule = new PermissionsRule(new String[]{Manifest.permission.RECORD_AUDIO});
+
+    /**
+     * 使用android emulator测试以下代码会出现严重回声
+     */
+    @Test
+    public void aec_test() {
+        // 开启扬声器
+        AudioManager audioManager=(AudioManager) InstrumentationRegistry.getTargetContext().getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+        audioManager.setSpeakerphoneOn(true);
+
+        final AudioTrack audioPlay ;
+        final AudioRecord audioRecord ;
+        Thread thread;
+
+        final int frequency = 8000;
+        final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+        final LinkedList<short[]> listAudio = new LinkedList<short[]>();
+
+        int bufferSize = android.media.AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_OUT_MONO, audioEncoding);
+        int bufferSizeRec = android.media.AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, audioEncoding);
+        audioRecord = new AudioRecord(
+                MediaRecorder.AudioSource.VOICE_COMMUNICATION , frequency, AudioFormat.CHANNEL_IN_MONO,
+                audioEncoding,	bufferSizeRec );
+
+        audioRecord.startRecording();
+
+        audioPlay = new AudioTrack(AudioManager.STREAM_VOICE_CALL, 8000, AudioFormat.CHANNEL_OUT_MONO, audioEncoding, bufferSize,
+                AudioTrack.MODE_STREAM,audioRecord.getAudioSessionId());
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                boolean playStart = false;
+                short data [] = new short[640];
+                int	bufferRead;
+                while (true) {
+                    bufferRead = audioRecord.read(data, 0, data.length);
+                    short out [] = new short[bufferRead];
+                    System.arraycopy(data, 0, out, 0, bufferRead);
+                    listAudio.add(out);
+                    if (playStart) {
+                        data = listAudio.removeFirst();
+                        audioPlay.write(data, 0, data.length);
+                    }
+                    else {
+                        if (listAudio.size() > 5) {
+                            playStart = true;
+                            audioPlay.play();
+                        }
+                    }
+                }
+            }
+        });
+        thread.start();
+
+        System.out.println("");
+
+        audioRecord.stop();
+        audioRecord.release();
+        audioPlay.stop();
+        audioPlay.release();
+    }
+}
---
+++ aec/app/src/androidTest/java/com/xy/android/aec/PermissionsRule.java
@@ -0,0 +1,57 @@
+package com.xy.android.aec;
+
+/**
+ * Created by john on 2018/6/22.
+ */
+
+import android.os.Build;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * This rule adds selected permissions to test app
+ */
+
+public class PermissionsRule implements TestRule {
+
+    private final String[] permissions;
+
+    public PermissionsRule(String[] permissions) {
+        this.permissions = permissions;
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                allowPermissions();
+                base.evaluate();
+                revokePermissions();
+            }
+        };
+    }
+
+    private void allowPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : permissions) {
+                InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
+                        "pm grant " + InstrumentationRegistry.getTargetContext().getPackageName()
+                                + " " + permission);
+            }
+        }
+    }
+
+    private void revokePermissions() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            for (String permission : permissions) {
+//                InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
+//                        "pm revoke " + InstrumentationRegistry.getTargetContext().getPackageName()
+//                                + " " + permission);
+//            }
+//        }
+    }
+}
